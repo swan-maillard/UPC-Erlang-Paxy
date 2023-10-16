d16d0a6893d857375c0dd26560d24071b435183b
Swan <maillard.swan@gmail.com>
2023-10-16 15:49:35 +0200

    First commit

---
+++ acceptor.erl
@@ -0,0 +1,65 @@
+-module(acceptor).
+-export([start/2]).
+
+start(Name, PanelId) ->
+  spawn(fun() -> init(Name, PanelId) end).
+        
+init(Name, PanelId) ->
+  Promised = order:null(), 
+  Voted = order:null(),
+  Value = na,
+  acceptor(Name, Promised, Voted, Value, PanelId).
+
+acceptor(Name, Promised, Voted, Value, PanelId) ->
+  %% We receive a PREPARE request from PROPOSER
+  receive
+    {prepare, Proposer, Round} ->
+
+      case order:gr(Round, Promised) of
+       %% If Round greater than current promise, we promise it
+        true ->
+            Proposer ! {promise, Round, Voted, Value},
+            io:format("[Acceptor ~w] Phase 1: promised ~w voted ~w colour ~w~n", [Name, Round, Voted, Value]),
+
+            % Update gui
+            Colour = case Value of na -> {0,0,0}; _ -> Value end,
+            PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Voted]), 
+                      "Promised: " ++ io_lib:format("~p", [Round]), Colour},
+            %% We update the ACCEPTOR values
+            acceptor(Name, Round, Voted, Value, PanelId);
+        %% Else
+        false ->
+          Proposer ! {sorry, {prepare, Round}},
+          %% We update the ACCEPTOR values (they didn't change)
+          acceptor(Name, Promised, Voted, Value, PanelId)
+      end;
+    %% We receive an ACCEPT request from PROPOSER
+    {accept, Proposer, Round, Proposal} ->
+      case order:goe(Round, Promised) of
+        %% If Round is greater or equal than the promise, we vote for it
+        true ->
+          Proposer ! {vote, Round},
+          case order:goe(Round, Voted) of
+            %% If Round (new vote) is greater or equal than the current vote, we update the vote
+            true ->
+              io:format("[Acceptor ~w] Phase 2: promised ~w voted ~w colour ~w~n", [Name, Promised, Round, Proposal]),
+              % Update gui
+              PanelId ! {updateAcc, "Voted: " ++ io_lib:format("~p", [Round]), 
+                        "Promised: " ++ io_lib:format("~p", [Promised]), Proposal},
+              %% We update the ACCEPTOR values
+              acceptor(Name, Promised, Round, Proposal, PanelId);
+            %% Else we keep the previous vote
+            false ->
+              %% We update the ACCEPTOR values (they didn't change)
+              acceptor(Name, Promised, Voted, Value, PanelId)
+          end;
+        %% Else                            
+        false ->
+          Proposer ! {sorry, {accept, Round}},
+          %% We update the ACCEPTOR values (they didn't change)
+          acceptor(Name, Promised, Voted, Value, PanelId)
+      end;
+    stop ->
+      PanelId ! stop,
+      ok
+  end.
---
+++ paxy.erl
@@ -0,0 +1,78 @@
+-module(paxy).
+-export([start/1, stop/0, stop/1]).
+
+-define(RED, {255,0,0}).
+-define(BLUE, {0,0,255}).
+-define(GREEN, {0,255,0}).
+
+-define(ACC, 'paxy-acc@LAPTOP-MC47CKNN').
+-define(PRO, 'paxy-pro@LAPTOP-MC47CKNN').
+
+% Sleep is a list with the initial sleep time for each proposer
+start(Sleep) ->
+  AcceptorNames = ["Marie-Josette", "Marie-Micheline", "Marie-Cunégonde", "Marie-Jeanne", "Marie-Elise"],
+  AccRegister = [{mariejosette, ?ACC}, {mariemicheline, ?ACC}, {mariecunegonde, ?ACC}, {mariejeanne, ?ACC}, {marieelise, ?ACC}],
+  ProposerNames = [{"Jean-Phillibert", ?RED}, {"Jean-Maxence", ?GREEN}, {"Jean-Eude", ?BLUE}],
+  PropInfo = [{{jeanphillibert, ?PRO}, ?RED}, {{jeanmaxence, ?PRO}, ?GREEN}, {{jeaneude, ?PRO}, ?BLUE}],
+  register(gui, spawn(fun() -> gui:start(AcceptorNames, ProposerNames) end)),
+  gui ! {reqState, self()},
+  receive
+    {reqState, State} ->
+      {AccIds, PropIds} = State,
+      spawn(?ACC, fun() -> start_acceptors(AccIds, AccRegister) end),
+      spawn(?PRO, fun() ->
+        Begin = erlang:monotonic_time(),
+        start_proposers(PropIds, PropInfo, AccRegister, Sleep, self()),
+        wait_proposers(length(PropIds)),
+        End = erlang:monotonic_time(),
+        Elapsed = erlang:convert_time_unit(End-Begin, native, millisecond),
+        io:format("[Paxy] Total elapsed time: ~w ms~n", [Elapsed])
+      end)
+  end.
+    
+start_acceptors(AccIds, AccReg) ->
+  case AccIds of
+    [] ->
+      ok;
+    [AccId|Rest] ->
+      [{RegName, ?ACC}|RegNameRest] = AccReg,
+      register(RegName, acceptor:start({RegName, ?ACC}, AccId)),
+      start_acceptors(Rest, RegNameRest)
+  end.
+
+start_proposers(PropIds, PropInfo, Acceptors, Sleep, Main) ->
+  case PropIds of
+    [] ->
+      ok;
+    [PropId|Rest] ->
+      [{{RegName, ?PRO}, Colour}|RestInfo] = PropInfo,
+      [FirstSleep|RestSleep] = Sleep,
+      proposer:start({RegName, ?PRO}, Colour, Acceptors, FirstSleep, PropId, Main),	
+      start_proposers(Rest, RestInfo, Acceptors, RestSleep, Main)
+  end.
+
+wait_proposers(0) ->
+  ok;
+wait_proposers(N) ->
+  receive
+    done ->
+      wait_proposers(N-1)
+  end.
+
+stop() ->
+  stop(mariejosette),
+  stop(mariemicheline),
+  stop(mariecunegonde),
+  stop(mariejeanne),
+  stop(marieelise),
+  stop(gui).
+
+stop(Name) ->
+  case whereis(Name) of
+    undefined ->
+      ok;
+    Pid ->
+      Pid ! stop
+  end.
+
+ 
